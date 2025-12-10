b8bfa040bf0a26bef477f7a4920b7d36603349b3
Rodriguez Jorge <rodriguezja10@gmail.com>
2025-12-10 19:44:07 -0300

    Se modifico el runtest para GitAction

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,4 @@
 import pytest
 
-# Lista de archivos de prueba a ejecutar
-test_files = [
-    "test/test_login.py",
-    "test/test_inventory.py",
-    "test/test_cart.py",
-    "test/test_cart_json.py",
-    "test/test_api_reqres.py"
-]
 
-# Argumentos para ejecutar las pruebas: Archivos + REPORT HTML
-pytest_args = test_files + ["--html=report.html", "--self-contained-html", "-v"]
-
-pytest.main(pytest_args)
+pytest.main("test/","--html=reports/report.html", "--self-contained-html", "-v")
